c455d965866aaf6090b0bf8fa4f3b24755d118ab
banguyenvareal <nguyen.huu.ba@vareal.vn>
2019-08-22 15:44:28 +0700

    resolve comment

--- app/assets/javascripts/request_images.js
+++ app/assets/javascripts/request_images.js
@@ -3,9 +3,10 @@ $(function(){
     $(this).click(function(){
       $('#main_image').attr('src', $(this).attr('src'));
       $('.small-img-style').css('border-width', '0px');
-      $(this).css('border-color', 'red');
-      $(this).css('border-style', 'solid');
-      $(this).css('border-width', '1px');
+      // $(this).css('border-color', 'red');
+      // $(this).css('border-style', 'solid');
+      // $(this).css('border-width', '1px');
+      $(this).addClass('border-red');
     });
   });
 });
