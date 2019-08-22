f1627a07a281689ad28467aa62c949ac76a2c223
banguyenvareal <nguyen.huu.ba@vareal.vn>
2019-08-22 17:58:35 +0700

    forgot-password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   resources :users, only: %i[new create show edit update] do
     resources :book_requests, controller: 'users/book_requests'
     resources :books, controller: 'users/books'
